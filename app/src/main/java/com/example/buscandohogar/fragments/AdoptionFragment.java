a623b9c8415659a65cf41231d0ab54ad9510ebfb
Pierre Laurens Florenz <plaurens@heinsohn.com.co>
2020-10-30 08:02:53 -0500

    Agregan iconos y se mejora interfaz

--- app/src/main/java/com/example/buscandohogar/fragments/AdoptionFragment.java
+++ app/src/main/java/com/example/buscandohogar/fragments/AdoptionFragment.java
@@ -7,8 +7,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.buscandohogar.R;
+
+import java.util.ArrayList;
 
 public class AdoptionFragment extends Fragment {
 
@@ -31,6 +35,7 @@ public class AdoptionFragment extends Fragment {
     }
 
     private void setDatos() {
+
 
     }
 }
